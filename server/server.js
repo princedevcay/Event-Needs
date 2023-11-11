7b3cc5cfb6a4098febb11df00f26d462941e7740
Prince Caleb <akakpo.c.benjamin@gmail.com>
2023-11-10 19:05:38 -0500

    Add root route

--- server/server.js
+++ server/server.js
@@ -1,5 +1,5 @@
 const app = require('./app');
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 10000;
 
 app.listen(port, () => {
   console.log(`Server listening at http://localhost:${port}`);
