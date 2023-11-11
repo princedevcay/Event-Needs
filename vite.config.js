5d1bb12e090af45487fac8cb3c0db7a3497ca498
Prince Caleb <akakpo.c.benjamin@gmail.com>
2023-11-10 19:19:53 -0500

    Change Build ChunkSizee

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    chunkSizeWarningLimit: 1000, // Limit in KBs
+  },
   plugins: [react()],
   server: {
     proxy: {
